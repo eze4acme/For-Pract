b89f1b6489758290ac6407b8e9f358e2b4ea0df3
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-20 19:44:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const span = document.getElementById('span')
 const button = document.getElementById('button')
 function countX() {
     let count = 0
-    console.log('Hi');
-    function mainFnc() {
-        return count ++
+    // console.log('Hi');
+   function mainFnc() {
+     return count = count + 1
     }
-  span.innerText = count
+    span.innerHTML = count
+    mainFnc()
 }
 
 button.addEventListener('click', countX)
