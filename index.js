16c7cc8e6c48d451edaf79832514dbd8abc16d88
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-20 19:45:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const span = document.getElementById('span')
+console.log(span);
 const button = document.getElementById('button')
 function countX() {
     let count = 0
     // console.log('Hi');
-   function mainFnc() {
+function mainFnc() {
      return count = count + 1
     }
     span.innerHTML = count
