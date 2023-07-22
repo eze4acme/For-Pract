e9f2c3d70c33a7f82704dc2a6c3509bf458ad405
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-22 20:36:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,52 @@ const button = document.getElementById('button')
 // return add
 // }
 // const count = counter()
-function createCounter() {
-  let count = 0; // Variable declared inside the closure function
+// function createCounter() {
+//   let count = 0; // Variable declared inside the closure function
 
-  function increment() {
-    count++;
-    console.log(`Current count: ${count}`);
-  }
+//   function increment() {
+//     count++;
+//     console.log(`Current count: ${count}`);
+//   }
 
-  return increment;
-}
+//   return increment;
+// }
 
 // Usage example:
-const counter = createCounter();
+// const counter = createCounter();
 
 // counter(); // Output: Current count: 1
 // counter(); // Output: Current count: 2
 // counter(); // Output: Current count: 3
 
-button.addEventListener('click', counter)
+// button.addEventListener('click', counter)
+
+// function num() {
+//     let num = 45
+//     console.log(num);
+//     function str() {
+//         console.log(`Hi ${num}`);
+//     }
+//     return str
+// }
+// const newb = num()
+// num()
+// num()
+// console.log(newb);
+// newb()
+
+
+
+function debounce() {
+    let timeId;
+return () => {
+    console.log(timeId);
+    clearTimeout(timeId)
+    console.log(timeId);
+     timeId = setTimeout(() => {
+    console.log('you cliked me');
+   }, 2000); 
+}
+}
+
+ button.addEventListener('click', debounce())
