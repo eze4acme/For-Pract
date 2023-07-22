838f14809b463ba57f05cbfb172fbf3b1daf7435
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-22 20:11:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const span = document.getElementById('span')
-
 const button = document.getElementById('button')
-function count() {
-    let count = 0;
-    
-function mainFnc() {
-     return count = count + 8
-    }
-    span.innerHTML = count
-    main()
+// console.log(span);
+// console.log(button);
+// function counter() {
+//     let count = 0
+//   function add() {
+//    span.textContent = count++
+//    console.log( count++);
+// }
+// return add
+// }
+// const count = counter()
+function createCounter() {
+  let count = 0; // Variable declared inside the closure function
+
+  function increment() {
+    count++;
+    console.log(`Current count: ${count}`);
+  }
+
+  return increment;
 }
 
-button.addEventListener('click', count)
+// Usage example:
+const counter = createCounter();
+
+// counter(); // Output: Current count: 1
+// counter(); // Output: Current count: 2
+// counter(); // Output: Current count: 3
+
+button.addEventListener('click', counter)
